4183abe3cc97af3d9ad6970c0a3b9f288aee832d
Steven Briscoe <me@stevenbriscoe.com>
2020-11-09 16:48:39 +0000

    Add Repo

---
+++ src/Repositories/ClusterIssuerRepository.php
@@ -0,0 +1,17 @@
+<?php namespace Maclof\Kubernetes\Repositories;
+
+use Maclof\Kubernetes\Collections\ClusterIssuerCollection;
+use Maclof\Kubernetes\Repositories\Strategy\PatchMergeTrait;
+
+class ClusterIssuerRepository extends Repository
+{
+    use PatchMergeTrait;
+
+    protected $uri = 'clusterissuers';
+
+    protected function createCollection($response)
+    {
+        return new ClusterIssuerCollection($response['items']);
+    }
+
+}
